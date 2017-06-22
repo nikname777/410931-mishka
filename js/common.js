f0f94d84d296fb8ce65c26973ec91ce97559f0cb
nikname777 <viabiznes@gmail.com>
2017-06-22 22:34:39 +0300

    Модальное окно не фурычит)

--- js/common.js
+++ js/common.js
@@ -1,8 +1,28 @@
-var menuOpen = document.querySelector(".modal");
+var menuOpen = document.querySelector(".mobile-menu");
 var menuPopup = document.querySelector(".main-nav__menu");
 
 menuOpen.addEventListener("click", function(event) {
   event.preventDefault();
   menuPopup.classList.toggle("main-nav__menu--open");
-  menuOpen.classList.toggle("modal-content-close");
+  menuOpen.classList.toggle("mobile-menu-close");
 });
+
+
+
+
+var buttonOpenForm = document.querySelector(".page-product__btn"),
+  overlay = document.querySelector(".modal__overlay"),
+  form = document.querySelector(".modal");
+
+buttonOpenForm.addEventListener("click", function() {
+  form.classList.add("modal__show-form");
+  overlay.classList.add("modal__show-form");
+});
+
+if(overlay) {
+  overlay.addEventListener("click", function(evt) {
+  form.classList.remove("modal__show-form");
+  overlay.classList.remove("modal__show-form");
+});
+}
+
