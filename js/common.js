cc266453cf485d4fd14462f35ad2ceff22cdf860
nikname777 <viabiznes@gmail.com>
2017-06-23 12:33:20 +0300

    Не появляется модоальное окно

--- js/common.js
+++ js/common.js
@@ -1,3 +1,5 @@
+// Меню на мобильной версии
+
 var menuOpen = document.querySelector(".mobile-menu");
 var menuPopup = document.querySelector(".main-nav__menu");
 
@@ -8,21 +10,21 @@ menuOpen.addEventListener("click", function(event) {
 });
 
 
+// Модальное окно добавления товара
 
-
-var buttonOpenForm = document.querySelector(".page-product__btn"),
-  overlay = document.querySelector(".modal__overlay"),
-  form = document.querySelector(".modal");
+var buttonOpenForm = document.querySelector(".page-product__btn");
+var overlay = document.querySelector(".modal__overlay");
+var form = document.querySelector(".modal");
 
 buttonOpenForm.addEventListener("click", function() {
   form.classList.add("modal__show-form");
-  overlay.classList.add("modal__show-form");
+  overlay.classList.add(".modal__overlay-show");
 });
 
 if(overlay) {
   overlay.addEventListener("click", function(evt) {
   form.classList.remove("modal__show-form");
-  overlay.classList.remove("modal__show-form");
+  overlay.classList.remove(".modal__overlay-show");
 });
 }
 
